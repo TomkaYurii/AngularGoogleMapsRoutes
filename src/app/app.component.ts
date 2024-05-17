bc73d01f84dca842fabf5b0ead1372cd8053f81c
TomkaYurii <tomka.yurii@gmail.com>
2024-05-17 15:40:28 +0300

    2024-05-17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  PlaceAutocompleteComponent,
+  PlaceSearchResult,
+} from "./components/place-autocomplete/place-autocomplete.component";
+import {CommonModule} from "@angular/common";
+import {MatToolbar} from "@angular/material/toolbar";
+import {PlaceDetailsComponent} from "./components/place-details/place-details.component";
+import {MapDisplayComponent} from "./components/map-display/map-display.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule,
+    MatToolbar,
+
+    PlaceAutocompleteComponent,
+    PlaceDetailsComponent,
+    MapDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularExampleRoutes';
+
+  fromValue: PlaceSearchResult = { address: '' };
+  toValue: PlaceSearchResult = { address: '' };
 }
